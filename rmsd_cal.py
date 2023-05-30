b58746f927accecc3cb48f3f74fbae250042c1ab
tieulongphan8995 <78349942+tieulongphan8995@users.noreply.github.com>
2023-05-30 20:30:53 +0700

    Create rmsd_cal.py

---
+++ rmsd_cal.py
@@ -0,0 +1,20 @@
+def get_inplace_rmsd (ref,target):
+    
+    r=rdFMCS.FindMCS([ref,target])
+    
+    a=ref.GetSubstructMatch(Chem.MolFromSmarts(r.smartsString))
+    b=target.GetSubstructMatch(Chem.MolFromSmarts(r.smartsString))   
+    amap=list(zip(a,b))
+    
+    distances=[]
+    for atomA, atomB in amap:
+        pos_A=ref.GetConformer().GetAtomPosition (atomA)
+        pos_B=target.GetConformer().GetAtomPosition (atomB)
+        coord_A=np.array((pos_A.x,pos_A.y,pos_A.z))
+        coord_B=np.array ((pos_B.x,pos_B.y,pos_B.z))
+        dist_numpy = np.linalg.norm(coord_A-coord_B)        
+        distances.append(dist_numpy)
+        
+    rmsd=math.sqrt(1/len(distances)*sum([i*i for i in distances]))
+    
+    return rmsd
